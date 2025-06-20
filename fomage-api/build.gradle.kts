59a4a82af9f6bb66dc69a216da46b5c3c7be96b3
kigawa <kigawa.8390@gmail.com>
2025-06-20 15:23:08 +0900

    wp

--- fomage-api/build.gradle.kts
+++ fomage-api/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("fomage.common")
+    alias(libs.plugins.spring.boot) apply false
+    alias(libs.plugins.spring.dependency.management)
 }
 
 repositories {
@@ -7,10 +9,19 @@ repositories {
 }
 
 dependencies {
+    // Module dependencies
+    implementation(project(":fomage-core"))
+
     // Kotlin standard library
     implementation(libs.kotlin.stdlib)
-    
+
+    // Spring Boot
+    implementation(libs.spring.boot.starter)
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.boot.starter.data.mongodb)
+
     // Testing
     testImplementation(libs.kotlin.test)
     testImplementation(libs.kotlin.test.junit)
+    testImplementation(libs.spring.boot.starter.test)
 }
--- fomage/build.gradle.kts
+++ fomage/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("fomage.app")
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
 }
 
 repositories {
@@ -9,10 +11,15 @@ repositories {
 dependencies {
     // Module dependencies
     implementation(project(":fomage-api"))
+    implementation(project(":fomage-core"))
+    implementation(project(":fomage-web"))
 
     // Kotlin standard library and coroutines
     implementation(libs.kotlin.stdlib)
     implementation(libs.kotlinx.coroutines.core)
+
+    // Spring Boot
+    implementation(libs.spring.boot.starter)
 
     // MongoDB
     implementation(libs.mongodb.driver)
@@ -23,13 +30,15 @@ dependencies {
 
     // Logging
     implementation(libs.logback)
+    implementation(libs.logback.core)
 
     // Testing
     testImplementation(libs.kotlin.test)
     testImplementation(libs.kotlin.test.junit)
     testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.spring.boot.starter.test)
 }
 
 application {
-    mainClass.set("net.kigawa.fomage.Main")
+    mainClass.set("net.kigawa.fomage.FomageApplication")
 }
